8cc66a8202a51a57f810ea6151a5887f55d9be43
kyleduo <kyleduo@gmail.com>
2015-11-05 21:46:54 +0800

    update custom

--- SwitchButton/demo/src/main/java/com/kyleduo/switchbutton/demo/StyleActivity.java
+++ SwitchButton/demo/src/main/java/com/kyleduo/switchbutton/demo/StyleActivity.java
@@ -2,12 +2,13 @@ package com.kyleduo.switchbutton.demo;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.CompoundButton;
 
 import com.kyleduo.switchbutton.SwitchButton;
 
 public class StyleActivity extends ActionBarActivity {
 
-	private SwitchButton mChangeSb;
+	private SwitchButton mChangeSb, mFlymeSb, mMiuiSb;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +16,16 @@ public class StyleActivity extends ActionBarActivity {
 		setContentView(R.layout.activity_style);
 
 		mChangeSb = (SwitchButton) findViewById(R.id.sb_code);
+		mFlymeSb = (SwitchButton) findViewById(R.id.sb_custom_flyme);
+		mMiuiSb = (SwitchButton) findViewById(R.id.sb_custom_miui);
+
+		mChangeSb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				mFlymeSb.setEnabled(isChecked);
+				mMiuiSb.setEnabled(isChecked);
+			}
+		});
 	}
 
 }
