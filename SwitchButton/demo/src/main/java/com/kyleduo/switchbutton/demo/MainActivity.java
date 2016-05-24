9016d336ddf4dd67d72e38f25b0db3d544d47aee
kyleduo <kyleduo@gmail.com>
2016-05-24 22:34:41 +0800

    update

--- SwitchButton/demo/src/main/java/com/kyleduo/switchbutton/demo/MainActivity.java
+++ SwitchButton/demo/src/main/java/com/kyleduo/switchbutton/demo/MainActivity.java
@@ -2,10 +2,8 @@ package com.kyleduo.switchbutton.demo;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
